c16a1a8c0e0b2c88c8f6c18c3961c495704d0964
Mega-PC <tlijanihachem4@gmail.com>
2024-02-14 17:41:18 +0100

    add auto fill  extention

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,8 +17,8 @@ export class AppComponent {
     });
 
     function fillForm() {
-      const inputSelectors = ['input[name="ctl00$body$date_misecirculation"]', 'input[name="ctl00$body$date_acquisition"]'];
-      const values = ['20/02/1998', '20/02/1998'];
+      const inputSelectors = ['input[id="ctl00_body_date_misecirculation"]', 'input[id="ctl00_body_date_acquisition"]','input[id="ctl00_body_d_fin_assurance_vehicule"]'];
+      const values = ['20/02/1998', '26/02/1920','15/08/4450'];
 
       inputSelectors.forEach((selector, index) => {
         const input = document.querySelector(selector) as HTMLInputElement;
